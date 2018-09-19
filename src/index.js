5ee1c2689682898e49bcfcfa3b79df562acf7068
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-19 23:59:07 +0300

    Sorter soulution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.instanceSorter = [];
   }
 
   add(element) {
-    // your implementation
+    this.instanceSorter.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.instanceSorter[index];
   }
 
   get length() {
-    // your implementation
+    return this.instanceSorter.length;
   }
 
   toArray() {
-    // your implementation
+    return this.instanceSorter;
   }
 
   sort(indices) {
-    // your implementation
+    var arrNew = [];
+    
+    for (var i=0; i < indices.length; i++){
+      arrNew.push(this.instanceSorter[indices[i]]);
+      
+    }
+    if(!this.compareFunction){
+      arrNew.sort(function (a, b) {
+      return a - b;
+      });
+    }
+    else {
+      arrNew.sort(this.compareFunction);
+    }
+    
+    indices.sort(function(x, y) {
+      return x - y;
+    });
+    for (var k=0; k < indices.length; k++) {
+      this.instanceSorter.splice(indices[k], 1, arrNew[k]);
+    }
+    return this.instanceSorter;
   }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
-}
+  setComparator(compareFunction){
+    return this.compareFunction = compareFunction;
+  };
+};
 
 module.exports = Sorter;
